5429b0277245187a45f5443f8689ffeb29445fbd
Van Aerschot Bart <vanaerschotb@hotmail.com>
2019-05-15 20:08:29 +0200

    got rid of json, replaced it with arrays = nice and smooth algorithm

---
+++ translationEN.js
@@ -0,0 +1,60 @@
+var translationEN = [
+    [
+        "About me",
+		"Skills",
+		"Personal",
+		"Contact"
+    ],
+    [
+        "Hello there!",
+        "As you may have noticed above, my name is Van Aerschot Bart, and I'm a junior web-developer. As I'm just starting my professional carreer in this branche, you probably never heard of me before. Well, here's a chance to change that! A short introduction: I'm 34 years old, father to a 7 year old daughter (my pride and joy!) living in Opglabbeek, Limburg. My main language in coding is PHP, but I also have a little crush on JavaScript. To be honest, I enjoy almost every aspect of building a website in it's full, and when my knowledge of a language is a little short, I see it as a challenge and an opportunity to learn! I master most of what I need to know to create a beautiful fully-working website but I'm looking for a company who wants to help me fine-tune and upgrade my skills. At first so I can keep growing and improving, and secondly, that one day, I can be the one helping and teaching new colleagues to become better than they ever could have dreamed of.",
+        "Get to know me better!"
+    ],
+    [
+        "Skills",
+        [
+            "Languages",
+            "Coding",
+            "Academic",
+            "Extra's"
+        ],
+        [
+            "Dutch: native language",
+            "English: exellent",
+            "French: basics"
+        ],
+        [
+            "Programmatie-logica",
+            "Gegevens-analyse",
+            "OOP",
+            "SQL",
+            "HTML5",
+            "CSS3",
+            "JavaScript",
+            "PHP7",
+            "SCRUM",
+            "Bootstrap",
+            "GIT"
+        ],
+        [
+            "PHP-ontwikkelaar, VDAB Haasrode/Genk (2018-2019)",
+            "Web-development, thuis, zelf-studie (2017)",
+            "Automechaniek, CTT Genk (2003-2006)",
+            "Lassen/staal-constructies, TISM Bree (2000-2004)"
+        ],
+        [
+            "Rijbewijs: B",
+            "In bezit van eigen veroer",
+            "Gekwalificeerd in EHBO (via Rode Kruis Vlaanderen)",
+            "Mijn koffie is serieus goed!"
+        ]
+    ],
+    [
+        "Persoonlijk",
+        "Zoals u waarschijnlijk al had kunnen raden, er is meer aan mij dan programmeren en genieten van de tijd met mijn dochter in het weekend. Ten eerste ben ik een zeer sociaal persoon die liever nieuwe dingen verkend of vrienden bezoekt dan thuis te zitten. Dit is waarom ik hield van mijn tijd bij KAJ Louwel, een lokale jeugdvereniging, waar ik ook mijn eerste ervaring met leiderschap heb opgedaan toen ik 18 werd. Maar omdat ik weet dat ik niet elke avond weg kan gaan en mijn vrienden kan lastig vallen, heb ik een paar hobby's opgepikt.	Een van deze is een goed boek lezen, vooral een thriller met een beetje horror er in gemengd kan me alles rondom mij laten vergeten voor een paar uren.	Een fijne wandeling in de natuur doet het tegenovergestelde voor mij, elke 10 minuten een korte pauze nemen om mijn omgeving echt goed op te nemen kan mijn batterij sneller opladen dan wat dan ook! Mijn liefde voor natuur strekt zich ook uit tot alle dieren groot en klein (behalve muggen en wespen natuurlijk) en het maakt me ook bewust van de frigiliteit van ons eco-systeem dus in deze lijn, probeer ik zoveel te recycleren en mijn ecologische voetafdruk zo klein mogelijk te houden als het kan. Op het einde, denk ik, ben ik een redelijk normaal persoon die van zijn eigen dingen houdt en de anderen van hun dingen laat genieten maar altijd open staat om iets nieuws te ontdekken. En als een teken van waardering om uw tijd te nemen om te lezen wat ik geschreven heb, presenteer ik u één van mijn favoriete citaten."
+    ],
+    [
+        "Contacteer mij!",
+        "Dank u om alles te lezen tot helemaal hier beneden! Ik hoop dat ik een beetje interessant was. En als u mij en mijn website echt leuk vindt, dan kan u recht onder deze paragraaf een paar manieren vinden om mij te contacteren! Natuurlijk sta ik ook open voor vragen, moest u er nog hebben, dan zal ik meer dan blij zijn om deze te beantwoorden."
+    ]
+];
---
+++ translationNL.js
@@ -0,0 +1,60 @@
+var translationNL = [
+    [
+        "Over mij",
+		"Vaardigheden",
+		"Persoonlijk",
+		"Contact"
+    ],
+    [
+        "Hallo!",
+        "Zoals u waarschijnlijk hier boven al gemerkt hebt, mijn naam is Van Aerschot Bart, en ik ben een student webdeveloper.	Omdat mijn professionele carriére in deze sector nog in de startblokken staat, heeft u waarschijnlijk nog nooit van mij gehoord. Echter, hier is een kans om daar verandering in te brengen! Een korte introductie: ik ben 34 jaar oud, vader van een 7 jaar oude dochter (mijn trots en vreugde!), woonachtig te Opglabbeek, Limburg. Mijn hoofdtaal in het programmeren is PHP, maar ik heb ook een zwakke plek voor JavaScript.	Om eerlijk te zijn, ik geniet van zowat elk aspect in het uitbouwen van een website in zijn geheel, en wanneer mijn kennis over een programmeertaal te kort schiet, zie ik dat als een uitdaging en een kans om bij te leren! Ik beheers het meeste van wat ik nodig heb om een mooie, volledig werkende website te maken maar ik ben nog op zoek naar een bedrijf wat mij wilt helpen mijn vaardigheden op punt te stellen en te opwaarderen. Ten eerste zodat ik kan blijven groeien en verbeteren, en ten tweede, dat op een dag, ik degene ben die mijn nieuwe collega's kan helpen en bijleren om beter te worden dan ze ooit hadden durven dromen!",
+        "Leer mij beter kennen!"
+    ],
+    [
+        "Vaardigheden",
+        [
+            "Talen",
+            "Coderen",
+            "Academisch",
+            "Extra's"
+        ],
+        [
+            "Nederlands: moedertaal",
+            "Engels: zeer goed",
+            "Frans: basis"
+        ],
+        [
+            "Programmatie-logica",
+            "Gegevens-analyse",
+            "OOP",
+            "SQL",
+            "HTML5",
+            "CSS3",
+            "JavaScript",
+            "PHP7",
+            "SCRUM",
+            "Bootstrap",
+            "GIT"
+        ],
+        [
+            "PHP-ontwikkelaar, VDAB Haasrode/Genk (2018-2019)",
+            "Web-development, thuis, zelf-studie (2017)",
+            "Automechaniek, CTT Genk (2003-2006)",
+            "Lassen/staal-constructies, TISM Bree (2000-2004)"
+        ],
+        [
+            "Rijbewijs: B",
+            "In bezit van eigen veroer",
+            "Gekwalificeerd in EHBO (via Rode Kruis Vlaanderen)",
+            "Mijn koffie is serieus goed!"
+        ]
+    ],
+    [
+        "Persoonlijk",
+        "Zoals u waarschijnlijk al had kunnen raden, er is meer aan mij dan programmeren en genieten van de tijd met mijn dochter in het weekend. Ten eerste ben ik een zeer sociaal persoon die liever nieuwe dingen verkend of vrienden bezoekt dan thuis te zitten. Dit is waarom ik hield van mijn tijd bij KAJ Louwel, een lokale jeugdvereniging, waar ik ook mijn eerste ervaring met leiderschap heb opgedaan toen ik 18 werd. Maar omdat ik weet dat ik niet elke avond weg kan gaan en mijn vrienden kan lastig vallen, heb ik een paar hobby's opgepikt.	Een van deze is een goed boek lezen, vooral een thriller met een beetje horror er in gemengd kan me alles rondom mij laten vergeten voor een paar uren.	Een fijne wandeling in de natuur doet het tegenovergestelde voor mij, elke 10 minuten een korte pauze nemen om mijn omgeving echt goed op te nemen kan mijn batterij sneller opladen dan wat dan ook! Mijn liefde voor natuur strekt zich ook uit tot alle dieren groot en klein (behalve muggen en wespen natuurlijk) en het maakt me ook bewust van de frigiliteit van ons eco-systeem dus in deze lijn, probeer ik zoveel te recycleren en mijn ecologische voetafdruk zo klein mogelijk te houden als het kan. Op het einde, denk ik, ben ik een redelijk normaal persoon die van zijn eigen dingen houdt en de anderen van hun dingen laat genieten maar altijd open staat om iets nieuws te ontdekken. En als een teken van waardering om uw tijd te nemen om te lezen wat ik geschreven heb, presenteer ik u één van mijn favoriete citaten."
+    ],
+    [
+        "Contacteer mij!",
+        "Dank u om alles te lezen tot helemaal hier beneden! Ik hoop dat ik een beetje interessant was. En als u mij en mijn website echt leuk vindt, dan kan u recht onder deze paragraaf een paar manieren vinden om mij te contacteren! Natuurlijk sta ik ook open voor vragen, moest u er nog hebben, dan zal ik meer dan blij zijn om deze te beantwoorden."
+    ]
+];
