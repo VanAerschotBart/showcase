0e096b7044afd916b7acc40d1bd7ede99106861f
VanAerschotBart <vanaerschotb@hotmail.com>
2019-05-20 15:47:42 +0200

    shortened my function by half (lost the double code)

--- startbootstrap-creative-gh-pages/js/custom.js
+++ startbootstrap-creative-gh-pages/js/custom.js
@@ -1,66 +1,51 @@
-var lang = "EN";  //needs to be declared so it can be used, and since the innerHTML is in English... 
-        
-function translator3000() {  
-        
-    var transButton = document.getElementById("myButton");
+var lang = "EN";  //needs to be declared so it can be used, and since the innerHTML is in English...
+
+function chooseYourSide(transArr) {
+    
     var transConList = document.getElementsByClassName("transCon");  //get every translation container
-            
-    if(lang == "EN") {
-        transButton.className = "langBtn UK";  //to set the correct background (flag) of the "button"
-        lang = "NL";
-                
-        for(var a=0; a<transConList.length; a++) {
+    
+    for(var a=0; a<transConList.length; a++) {
                     
-            var transList = transConList[a].getElementsByClassName("translate");  //get the elements in need of translation
+        var transList = transConList[a].getElementsByClassName("translate");  //get the elements in need of translation
                     
-            for(var b = 0; b<translationNL[a].length; b++) {
+        for(var b = 0; b<transArr[a].length; b++) {
                         
-                if(Array.isArray(translationNL[a][b])) {  //if it's an array, it means we're at the skills list
+            if(Array.isArray(transArr[a][b])) {  //if it's an array, it means we're at the skills list
                         
-                    var skillsList = transConList[a].getElementsByTagName("li");
+                var skillsList = transConList[a].getElementsByTagName("li");
                             
-                    for(var c=0; c<skillsList.length; c++){
-                        skillsList[c].innerHTML =  translationNL[a][b][c];
-                    }
+                for(var c=0; c<skillsList.length; c++){
+                        skillsList[c].innerHTML =  transArr[a][b][c];
+                }
                             
-                }
-                else{
-                    transList[b].innerHTML =  translationNL[a][b];
-                }
             }
-                    
+            else{
+                transList[b].innerHTML =  transArr[a][b];
+            }
         }
+                    
+    }
+    
+}
+        
+function translator3000() {
+    
+    var transButton = document.getElementById("myButton");
+    
+    if(lang == "EN") {
+        transButton.className = "langBtn UK";
+        chooseYourSide(translationNL);
+        lang = "NL";
     }
     else if(lang =="NL") {
         transButton.className = "langBtn NL";
+        chooseYourSide(translationEN);
         lang = "EN";
-                
-        for(var a=0; a<transConList.length; a++) {
-                    
-            var transList = transConList[a].getElementsByClassName("translate");
-                    
-            for(var b = 0; b<translationNL[a].length; b++) {
-                        
-                if(Array.isArray(translationEN[a][b])) {
-                            
-                    var skillsList = transConList[a].getElementsByTagName("li");
-                            
-                    for(var c=0; c<skillsList.length; c++){
-                        skillsList[c].innerHTML =  translationEN[a][b][c];
-                    }
-                            
-                }
-                else {
-                    transList[b].innerHTML =  translationEN[a][b];
-                }
-            }
-                    
-        }
     }
     else {
         alert("var lang error");
     }
-            
+      
 }
 
 //building a translation "button"
